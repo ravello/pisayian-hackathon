ef815147cccba6aeb8f5f8f38f64b1622ab50838
ravello <71367198+ravello@users.noreply.github.com>
2025-10-04 09:02:10 -0500

    added handleUpload function for upload button, basic display for currently selected file, fixed some logic

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,22 +2,42 @@
 import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/header.tsx"
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  // store the file input DOM element
   const fileInputRef = useRef(null);
 
+  // create state to store the file after selection
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  // file selection button
   const handleClick = () => {
     // trigger the hidden file input
     fileInputRef.current.click();
   };
 
+  // triggered when a file is selected
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      // convert the file to Pisayian database format
+      setSelectedFile(file);
+      console.log("Selected file:", file.name);
     }
   };
+
+  // file upload button
+  // handles upload logic and POST to backend
+  const handleUpload = () => {
+    if (!selectedFile) {
+      alert('Please select a file first.');
+      return;
+    }
+
+    // convert the file to Pisayian database format
+    // TODO
+  };
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-gradient-to-b from-yellow-50 to-red-50">
       <div className="fixed top-0 items-center justify-center">
@@ -53,9 +73,12 @@ export default function Home() {
               Pick a file
         </button>
         </p>
+        {selectedFile && (
+            <p>Selected: {selectedFile.name}</p> 
+        )}
         </div>
  
-        <button className="flex ml-3 px-4 py-1 transition-colors border border-solid border-black/[0.2]  text-black rounded-full hover:bg-[#f2f2f2] hover:border-black/[0.0]"> 
+        <button className="flex ml-3 px-4 py-1 transition-colors border border-solid border-black/[0.2]  text-black rounded-full hover:bg-[#f2f2f2] hover:border-black/[0.0]" onClick={handleUpload}> 
             Upload
         </button>
     </main>
